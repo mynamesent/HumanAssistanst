16d994d01be90996c1cd3ff8f634a517f3aa4a8c
Book <Book@DESKTOP-154TF5T>
2018-02-15 22:41:31 +0700

    Edit HumanServlet

--- src/main/java/Servlet/HumanServlet.java
+++ src/main/java/Servlet/HumanServlet.java
@@ -34,7 +34,7 @@ public class HumanServlet extends HttpServlet {
          HelloClass cl = new HelloClass();
         String message = cl.pullMessage(1);
         request.setAttribute("hellomessage", message);
-        getServletContext().getRequestDispatcher("/home.jsp").forward(request, response);
+        getServletContext().getRequestDispatcher("/index.jsp").forward(request, response);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
